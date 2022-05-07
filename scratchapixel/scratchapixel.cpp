05b599638be2a5574db123ec7d5e5cba3e979b4c
02rasjac <rasmusjacklin2002@gmail.com>
2022-05-07 22:02:43 +0200

    Add basic vectormathematics

--- scratchapixel/scratchapixel.cpp
+++ scratchapixel/scratchapixel.cpp
@@ -5,7 +5,19 @@
 int main() {
     point3i intVec(1);
     vec3f floVec(1.2f, -1.7f, 0.0f);
-    vec3d douVec(0.0, 5.2, -2.3);
+    vec3d douVec1(1.0, 5.0, -2.0);
+    vec3d douVec2(2.0, 2.0, 3.0);
 
-    std::cout << intVec << floVec << douVec << std::endl;
+    auto c1 = cross(douVec1, douVec2);
+    auto c2 = douVec1.cross(douVec2);
+    auto v1 = -douVec1;
+    auto v2 = douVec1 + douVec2;
+    auto v3 = douVec1 - douVec2;
+    std::cout << intVec << floVec << douVec1 << std::endl;
+    std::cout << floVec.normalize() << std::endl;
+    std::cout << dot(douVec1, douVec2) << std::endl;
+    std::cout << douVec1.dot(douVec2) << std::endl;
+    std::cout << c1 << std::endl;
+    std::cout << c2 << std::endl;
+    std::cout << v1 << ' ' << v2 << ' ' << v3 << std::endl;
 }
--- scratchapixel/vec3.h
+++ scratchapixel/vec3.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 template<typename T>
 class vec3 {
@@ -8,6 +9,35 @@ public:
 	vec3() : x(0), y(0), z(0) {};
 	vec3(const T& xx) : x(xx), y(xx), z(xx) {};
 	vec3(const T& xx, const T& yy, const T& zz) : x(xx), y(yy), z(zz) {};
+	T length() const {
+		return sqrt(x * x + y * y + z * z);
+	}
+	T lengthSquared() const {
+		return x * x + y * y + z * z;
+	};
+	/*
+	* Normalize this vector
+	* @return A reference to this vector after it been normalized
+	*/
+	vec3<T>& normalize() {
+		T len = length();
+		// Prevent division by zero
+		if (len > 0) {
+			T invLength = 1 / length();
+			x *= invLength, y *= invLength, z *= invLength;
+		}
+		return *this;
+	}
+	T dot(const vec3<T> u) const {
+		return x * u.x + y * u.y + z * u.z;;
+	}
+	vec3<T> cross(const vec3<T>& u) {
+		return vec3<T>(
+			y * u.z - z * u.y,
+			z * u.x - x * u.z,
+			x * u.y - y * u.x
+		);
+	}
 public:
 	T x, y, z;
 };
@@ -20,14 +50,56 @@ typedef vec3<double> vec3d, point3d;
 // Utility functions
 // =================
 
-inline std::ostream& operator<<(std::ostream& out, vec3i& vec) {
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out, vec3<T>& vec) {
 	return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 }
 
-inline std::ostream& operator<<(std::ostream& out, vec3f& vec) {
-	return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+template<typename T>
+inline vec3<T> operator-(const vec3<T>& u) {
+	return vec3<T>(-u.x, -u.y, -u.z);
 }
 
-inline std::ostream& operator<<(std::ostream& out, vec3d& vec) {
-	return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+template<typename T>
+inline vec3<T> operator+(const vec3<T>& v, const vec3<T>& u) {
+	return vec3<T>(
+		v.x + u.x, 
+		v.y + u.y, 
+		v.z + u.z
+	);
+}
+
+template<typename T>
+inline vec3<T> operator-(const vec3<T>& v, const vec3<T>& u) {
+	return v + (-u);
+}
+
+template<typename T>
+inline vec3<T> operator*(const T t, const vec3<T> v) {
+	return vec3<T>(t * v.x, t * v.y, t * v.z);
+}
+
+template<typename T>
+inline vec3<T> operator*(const vec3<T> v, const double t) {
+	return t*v;
+}
+
+template<typename T>
+inline vec3<T> operator/(const vec3<T> v, const double t) {
+	double invt = 1 / t;
+	return invt * v;
+}
+
+template<typename T>
+inline T dot(const vec3<T>& v, const vec3<T>& u) {
+	return v.x * u.x + v.y * u.y + v.z * u.z;
+}
+
+template<typename T>
+inline vec3<T> cross(const vec3<T>& v, const vec3<T>& u) {
+	return vec3<T>(
+		v.y * u.z - v.z * u.y, 
+		v.z * u.x - v.x * u.z, 
+		v.x * u.y - v.y * u.x
+	);
 }
