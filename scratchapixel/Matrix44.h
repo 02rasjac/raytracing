c51e579fc565e35d5177f171135219cf43fba170
02rasjac <rasmusjacklin2002@gmail.com>
2022-05-10 18:36:20 +0200

    Add matrix-transformation to vectors

--- scratchapixel/Matrix44.h
+++ scratchapixel/Matrix44.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include "Vec3.h"
 
 template<typename T>
 class Matrix44 {
@@ -21,6 +22,40 @@ public:
 		}
 
 		return mult;
+	}
+	/*
+	* Perfom vector-matrix-multiplication, assuming the vector src = (x,y,z,1).
+	* @param src Vector to multiply the matrix with.
+	* @param dst Vector to save the result in.
+	*/
+	void multVecMat(const Vec3<T>& src, Vec3<T>& dst) const {
+		dst.x = src.x * m[0][0] + src.y * m[1][0] + src.z * m[2][0] + m[3][0];
+		dst.y = src.x * m[0][1] + src.y * m[1][1] + src.z * m[2][1] + m[3][1];
+		dst.z = src.x * m[0][2] + src.y * m[1][2] + src.z * m[2][2] + m[3][2];
+	}
+	/*
+	* Perfom vector-matrix-multiplication, taking into consideration the case of projection, i.e dst = (x', y', z' w').
+	* @param src Vector to multiply the matrix with.
+	* @param dst Vector to save the result in.
+	*/
+	void multVecMatProj(const Vec3<T>& src, Vec3<T>& dst) const {
+		multiVecMat(src, dst);
+		auto w = src.x * m[0][3] + src.y * m[1][3] + src.z * m[2][3] + m[3][3];
+		if (w != 1 && w != 0) {
+			dst.x /= w;
+			dst.y /= w;
+			dst.z /= w;
+		}
+	}
+	/*
+	* Perfom vector-matrix-multiplication, with no consideration for translation, only direction.
+	* @param src Vector to multiply the matrix with.
+	* @param dst Vector to save the result in.
+	*/
+	void multDirMat(const Vec3<T>& src, Vec3<T>& dst) const {
+		dst.x = src.x * m[0][0] + src.y * m[1][0] + src.z * m[2][0];
+		dst.y = src.x * m[0][1] + src.y * m[1][1] + src.z * m[2][1];
+		dst.z = src.x * m[0][2] + src.y * m[1][2] + src.z * m[2][2];
 	}
 private:
 	T m[4][4] = { {1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1} }; // Initialise with identitymatrix
